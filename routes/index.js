7c0f79cee91535a8f0ff88da6084c4ae11251baf
Eric Wharton <wharter1980@gmail.com>
2020-07-28 16:25:25 -0500

    update secrets

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.post("/register", (req, res) => {
         lastName: req.body.lastName,
         email: req.body.email
     });
-    if(req.body.adminCode === 'secretCode123'){
+    if(req.body.adminCode === process.env['SECRETCODE']){
         newUser.isAdmin = true;
     }
     User.register(newUser, req.body.password, (err, user)=>{
